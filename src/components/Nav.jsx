6216e12623ccdd2f67e3521ab2a7020c9636637f
yourcarrot0214 <stylenbs@gmail.com>
2022-09-07 18:08:25 +0900

    useLocation을 이용한 검색페이지 구현 및 UI

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,8 +1,16 @@
 import React, { useState, useEffect } from "react";
 import "./Nav.css";
+import { useNavigate } from "react-router-dom";
 
 export default function Nav() {
   const [show, handleShow] = useState(false);
+  const [searchValue, setSearchValue] = useState("");
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setSearchValue(e.target.value);
+    navigate(`/search?q=${e.target.value}`);
+  };
 
   useEffect(() => {
     window.addEventListener("scroll", () => {
@@ -23,7 +31,14 @@ export default function Nav() {
         src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/200px-Netflix_2015_logo.svg.png"
         alt="Netflix logo"
         className="nav__logo"
-        onClick={() => window.location.reload()}
+        onClick={() => (window.location.href = "/")}
+      />
+      <input
+        value={searchValue}
+        onChange={handleChange}
+        className="nav__input"
+        type="text"
+        placeholder="영화를 검색해주세요."
       />
       <img
         src="https://www.themoviedb.org/t/p/w300_and_h450_bestv2/xxYawgFO1woBRveH7WL9D1BxB4W.jpg"
--- src/pages/SearchPage/index.jsx
+++ src/pages/SearchPage/index.jsx
@@ -1,5 +1,69 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import axios from "../../api/axios";
+import "./SearchPage.css";
 
-export default function index() {
-  return <div>Search Page</div>;
+export default function SearchPage() {
+  const navigate = useNavigate();
+  const [searchResults, setSearchResults] = useState([]);
+
+  const useQuery = () => {
+    return new URLSearchParams(useLocation().search);
+  };
+
+  let query = useQuery();
+  const searchTerm = query.get("q");
+
+  const fetchSearchMovie = async (searchTerm) => {
+    try {
+      const request = await axios.get(
+        `/search/multi?include_adult=true&query=${searchTerm}`
+      );
+      console.log(request);
+      setSearchResults(request.data.results);
+    } catch (error) {
+      console.log("🕹 fetchSearchMovie Error : ", error);
+    }
+  };
+
+  useEffect(() => {
+    if (searchTerm) {
+      fetchSearchMovie(searchTerm);
+    }
+  }, [searchTerm]);
+
+  const renderSearchResults = () => {
+    return searchResults.length > 0 ? (
+      <section className="search-container">
+        {searchResults.map((movie) => {
+          if (movie.backdrop_path !== null && movie.media_type !== "person") {
+            const movieImageUrl =
+              "https://image.tmdb.org/t/p/w500" + movie.backdrop_path;
+            return (
+              <div className="movie" key={movie.id}>
+                <div
+                  onClick={() => navigate(`/${movie.id}`)}
+                  className="movie__column-poster"
+                >
+                  <img
+                    src={movieImageUrl}
+                    alt="movie"
+                    className="movie__poster"
+                  />
+                </div>
+              </div>
+            );
+          }
+        })}
+      </section>
+    ) : (
+      <section className="no-results">
+        <div className="no-results__text">
+          <p>찾고자하는 검색어"{searchTerm}"에 맞는 영화가 없습니다.</p>
+        </div>
+      </section>
+    );
+  };
+
+  return renderSearchResults();
 }
