c6897c042fc8359ddacb789286fccd10d42f118a
yourcarrot0214 <stylenbs@gmail.com>
2022-09-07 16:44:25 +0900

    넷플릭스 앱에 react-router-dom 적용하기

--- src/App.js
+++ src/App.js
@@ -1,35 +1,31 @@
-import logo from "./logo.svg";
+import { Outlet, Routes, Route } from "react-router-dom";
 import "./App.css";
-import Nav from "./components/Nav";
-import Banner from "./components/Banner";
-import Row from "./components/Row";
 import Footer from "./components/Footer";
-import requests from "./api/requests";
+import Nav from "./components/Nav";
+import DetailPage from "./pages/DetailPage";
+import MainPage from "./pages/MainPage";
+import SearchPage from "./pages/SearchPage";
+
+const Layout = () => {
+  return (
+    <div>
+      <Nav />
+      <Outlet />
+      <Footer />
+    </div>
+  );
+};
 
 function App() {
   return (
     <div className="App">
-      <Nav />
-      <Banner />
-      <Row
-        title="NETFLIX ORIGINALS"
-        id="NO"
-        fetchUrl={requests.fetchNetflixOriginals}
-        isLargeRow
-      />
-      <Row title="Trending Now" id="TN" fetchUrl={requests.fetchTrending} />
-      <Row title="Top Rated" id="TR" fetchUrl={requests.fetchTopRated} />
-      <Row
-        title="Action Movies"
-        id="AM"
-        fetchUrl={requests.fetchActionMovies}
-      />
-      <Row
-        title="Comedy Movies"
-        id="Cm"
-        fetchUrl={requests.fetchComedyMovies}
-      />
-      <Footer />
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<MainPage />} />
+          <Route path=":movieId" element={<DetailPage />} />
+          <Route path="search" element={<SearchPage />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
